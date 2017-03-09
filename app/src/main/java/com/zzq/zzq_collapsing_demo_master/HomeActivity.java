b4269b4407cd0fce73ed84131f5dac66f42b3d03
志强 <zzqtdos@gmail.com>
2017-03-10 00:06:25 +0800

    新建HomeActivity

---
+++ app/src/main/java/com/zzq/zzq_collapsing_demo_master/HomeActivity.java
@@ -0,0 +1,35 @@
+package com.zzq.zzq_collapsing_demo_master;
+
+import com.zzq.zzq_collapsing_demo_master.base.RxBaseActivity;
+import com.zzq.zzq_collapsing_demo_master.p.IPsersenter;
+
+/**
+ * Created by 志强 on 2017.3.10.
+ */
+
+public class HomeActivity extends RxBaseActivity<IPsersenter> {
+    @Override
+    public int getLayoutId() {
+        return 0;
+    }
+
+    @Override
+    protected void initVariables() {
+
+    }
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    protected void initDataAndLoadData() {
+
+    }
+
+    @Override
+    public IPsersenter getPersenter() {
+        return null;
+    }
+}
